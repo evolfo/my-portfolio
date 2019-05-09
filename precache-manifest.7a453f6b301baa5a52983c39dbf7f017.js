f04f8e96829814326647acca5750203b2732ddc4
Angelo Spampinato <spampinatodesign@gmail.com>
2019-05-09 15:54:43 -0400

    Updates

---
+++ precache-manifest.7a453f6b301baa5a52983c39dbf7f017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b423bd83737b68633837666bf75a9d",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "1ad2ed59056586f1c53d",
+    "url": "/my-portfolio/static/css/main.46fd53cf.chunk.css"
+  },
+  {
+    "revision": "ac89727e763831a930f5",
+    "url": "/my-portfolio/static/js/2.f8bb738e.chunk.js"
+  },
+  {
+    "revision": "1ad2ed59056586f1c53d",
+    "url": "/my-portfolio/static/js/main.0eac4dde.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
