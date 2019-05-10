7452f64c563ee1975bf9470091646b595bddc5b6
Angelo Spampinato <spampinatodesign@gmail.com>
2019-05-10 06:31:10 -0400

    Updates

---
+++ precache-manifest.8f838c502461ceec37b57d9453474a25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9704df09104ad992660bb8cc2805a909",
+    "url": "www.spampinato.dev/index.html"
+  },
+  {
+    "revision": "292e658daa183a525a6a",
+    "url": "www.spampinato.dev/static/css/main.46fd53cf.chunk.css"
+  },
+  {
+    "revision": "45f003186c4b329a1263",
+    "url": "www.spampinato.dev/static/js/2.7d8fa186.chunk.js"
+  },
+  {
+    "revision": "292e658daa183a525a6a",
+    "url": "www.spampinato.dev/static/js/main.759f2e02.chunk.js"
+  },
+  {
+    "revision": "ccc9cc1fd55cf2b9ab3f",
+    "url": "www.spampinato.dev/static/js/runtime~main.09c18dc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "www.spampinato.dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.da701854eb07f0414134c9d674620695.js"
+  "www.spampinato.dev/precache-manifest.8f838c502461ceec37b57d9453474a25.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("www.spampinato.dev/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
